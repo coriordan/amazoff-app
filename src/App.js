b5aef3fcc65d0ec459d666a92f4a37ce706abf19
Cathal O’Riordan <cathal@madein75.com>
2018-03-05 16:12:04 +0000

    added app header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const Header = () => {
+  return (
+    <header className="App-header">
+      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <a className="navbar-brand" href="#">Amazoff</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" 
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarCollapse" className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
 class AmazoffApp extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div className="view-container">
+        <Header />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
