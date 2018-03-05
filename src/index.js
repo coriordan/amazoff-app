3319f4e5de739fca772fae96805decf4c078730e
Cathal O’Riordan <cathal@madein75.com>
2018-03-05 16:11:21 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AmazoffApp from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+ReactDOM.render(<AmazoffApp />, 
+document.getElementById('root'));
