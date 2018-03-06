79f25039b7bb46aa1d7e31296e3f36bb0217e758
Cathal O’Riordan <cathal@madein75.com>
2018-03-06 08:57:36 +0000

    added basic layout for book list

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 const Header = () => {
   return (
-    <header className="App-header">
-      <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <header>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
         <a className="navbar-brand" href="#">Amazoff</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" 
         aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
@@ -27,9 +27,46 @@ class AmazoffApp extends Component {
     return (
       <div className="view-container">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container-fluid pt-4">
+          <div className="row">
+            <main className="col-md-8" role="main">
+              <h4>Best Sellers</h4>
+                <ul className="list-unstyled">
+                  <li className="media book my-4">
+                    <img className="book__image mr-3" src="/images/1.jpg" alt="Generic placeholder image"/>
+                    <div className="media-body">
+                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                  <li className="media book my-4">
+                    <img className="book__image mr-3" src="/images/2.jpg" alt="Generic placeholder image"/>
+                    <div className="media-body">
+                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                  <li className="media book my-4">
+                    <img className="book__image mr-3" src="/images/3.jpg" alt="Generic placeholder image"/>
+                    <div className="media-body">
+                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                  <li className="media book my-4">
+                    <img className="book__image mr-3" src="/images/4.jpg" alt="Generic placeholder image"/>
+                    <div className="media-body">
+                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                </ul>
+            </main>
+            <aside className="col-md-4">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </aside>
+          </div>
+        </div>
       </div>
     );
   }
